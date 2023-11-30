cf4d87317f28a7212b077ec45e695b6d02aeeed0
Jesus Adair Sosa Garcia <106420837+LGzzus@users.noreply.github.com>
2023-11-30 03:04:58 -0600

    Update functions.py

--- browser/functions.py
+++ browser/functions.py
@@ -5,22 +5,25 @@ class ResultadosClass:
         self.apariciones = apariciones
 
 def buscar_palabra(busqueda):
-    archivo_indice =r"https://github.com/LGzzus/buscador/blob/sandbox/browser/resultado.txt"
+    current_directory = os.path.dirname(__file__)
+    archivo_indice = os.path.join(current_directory, 'resultado.txt')
+    try:
+        with open(archivo_indice, 'r', encoding='utf-8') as archivo:
+            lineas = archivo.readlines()
+        for linea in lineas:
+            if busqueda + ':' in linea:
+                informacion = linea.replace(busqueda + ': ', '').strip()
+                try:
+                    informacion = ast.literal_eval(informacion)
+                    return informacion
+                except (ValueError, SyntaxError):
+                    return f'Error al procesar la información para la palabra "{busqueda}".'
 
-    with open(archivo_indice, 'r', encoding='utf-8') as archivo:
-        lineas = archivo.readlines()
+        return f'La palabra "{busqueda}" no se encuentra en el índice invertido.'
 
-    for linea in lineas:
-        if busqueda + ':' in linea:
-            informacion = linea.replace(busqueda + ': ', '').strip()
-            try:
-                informacion = ast.literal_eval(informacion)
-                return informacion
-            except (ValueError, SyntaxError):
-                return f'Error al procesar la información para la palabra "{busqueda}".'
-
-    return f'La palabra "{busqueda}" no se encuentra en el índice invertido.'
-
+    except FileNotFoundError:
+        return f'El archivo {archivo_indice} no se encuentra.'
+        
 def convertir_cadena_a_objetos(cadena):
     cadena_de_texto = str(cadena)
     print(f'Contenido de cadena_de_texto: {cadena}')

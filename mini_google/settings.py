70f220a966dfbe068fb94544238c3df8a83cda4f
Jesus Adair Sosa Garcia <106420837+LGzzus@users.noreply.github.com>
2023-11-30 02:36:57 -0600

    Update settings.py

--- mini_google/settings.py
+++ mini_google/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4#olan8nptg*h)b#b^@cx75_mf@2g2@-+5z-+76jnl5tvr)acw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['browser-ri.azurewebsites.net']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'browser'
 ]
 
 MIDDLEWARE = [

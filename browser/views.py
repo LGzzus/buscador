af771633a8b05a9c210e732257907ab8f3c5bac0
Jesus Adair Sosa Garcia <106420837+LGzzus@users.noreply.github.com>
2023-11-30 12:20:36 -0600

    Update views.py

--- browser/views.py
+++ browser/views.py
@@ -1,40 +1,38 @@
 from django.http import HttpResponse
 from django.template import Template, Context
 from browser.functions import buscar_palabra, convertir_cadena_a_objetos
-import os 
+import os
 
-def principal(request):
+def principal(request): # Vista Principal
     current_directory = os.path.dirname(__file__)
-    template_path = os.path.join(current_directory, 'plantillas', 'principal.html')
-
-    try:
-        with open(template_path, 'r') as template_file:
-            content = template_file.read()
-        plt_principal = Template(content)
-        ctx_principal = Context()
-        barraBusqueda = plt_principal.render(ctx_principal)
-        return HttpResponse(barraBusqueda)
-
-    except FileNotFoundError:
-        return HttpResponse("El archivo principal.html no se encuentra.")
+    template_path = os.path.join(current_directory,'plantillas','principal.html')
+    doc_palntilla_principal = open(template_path,'r')
+    plt_principal = Template(doc_palntilla_principal.read())
+    doc_palntilla_principal.close()
+    ctx_principal = Context()
+    barraBusqueda = plt_principal.render(ctx_principal)
+    return HttpResponse(barraBusqueda)
 
 def resultados(request):
     busqueda = request.GET.get('busqueda', '')
     resultado = buscar_palabra(busqueda)
+
+    # Verificar si resultado es una cadena antes de usar startswith
     if isinstance(resultado, str) and resultado.startswith('No hay resultados'):
+        # Si no hay resultados, devolver un mensaje simple.
         return HttpResponse('Ups, no hay resultados para la búsqueda: "{}"'.format(busqueda))
 
     lista_resultados = convertir_cadena_a_objetos(resultado)
     current_directory = os.path.dirname(__file__)
-    template_path = os.path.join(current_directory, 'plantillas', 'resultados.html')
+    template_path = os.path.join(current_directory,'plantillas','resultados.html')
+    # Resto del código para cargar y renderizar tu plantilla
+    doc_plantilla_resultados = open(template_path,'r')
+    plt_resultados = Template(doc_plantilla_resultados.read())
+    doc_plantilla_resultados.close()
+    ctx_resultados = Context({"lista_resultados": lista_resultados, "resultado": busqueda})
+    resultados = plt_resultados.render(ctx_resultados)
 
-    try:
-        with open(template_path, 'r') as template_file:
-            content = template_file.read()
-        plt_resultados = Template(content)
-        ctx_resultados = Context({"lista_resultado": lista_resultados, "resultado": busqueda})
-        resultados = plt_resultados.render(ctx_resultados)
-        return HttpResponse(resultados)
+    return HttpResponse(resultados)
 
     except FileNotFoundError:
         return HttpResponse("El archivo resultados.html no se encuentra.")
